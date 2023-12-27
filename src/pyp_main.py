727c46526d3d8fa01d3b075250dc304b289b6f61
Alberto Bartesaghi <alberto@cs.duke.edu>
2023-12-27 12:55:40 -0500

    Edit logger messages and add info message to show method used for movie frame alignment

--- src/pyp_main.py
+++ src/pyp_main.py
@@ -1182,6 +1182,7 @@ def spr_swarm(project_path, filename, debug = False, keep = False, skip = False 
                     gain_reference_file = project_params.resolve_path(parameters["gain_reference"])
                     if os.path.exists(gain_reference_file):
                         shutil.copy2(gain_reference_file, os.getcwd())
+                logger.info("Aligning frames using " + parameters['movie_ali'])
                 aligned_average = align.align_movie_super(
                     parameters, name, extension
                 )
