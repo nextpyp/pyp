9981103cb91d8f4a0137301fd78faf80b625b395
Alberto Bartesaghi <alberto@cs.duke.edu>
2024-05-25 00:52:59 -0400

    Force recalculation of particle picking  when updating tilt-series alignments or reconstruction

--- src/pyp/system/project_params.py
+++ src/pyp/system/project_params.py
@@ -881,6 +881,7 @@ def parameter_force_check(previous_parameters, new_parameters, project_dir="."):
                     new_parameters["tomo_vir_force"] = True
                     clean_tomo_vir_particles(project_dir)
                     new_parameters["detect_force"] = True
+                    new_parameters["ctf_force"] = True
 
                 elif "tomo_rec" in k:
                     logger.info(
