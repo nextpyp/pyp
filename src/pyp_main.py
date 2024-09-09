8f10bb4cf947f4f1043e79ff3c9f50f1a7978d1d
Alberto Bartesaghi <alberto@cs.duke.edu>
2024-09-09 14:35:52 -0400

    Always send particles metadata to website

--- src/pyp_main.py
+++ src/pyp_main.py
@@ -1758,10 +1758,8 @@ def tomo_swarm(project_path, filename, debug = False, keep = False, skip = False
         if len(virion_coordinates) > 0:
             logger.info(f"Total number of virions = {len(virion_coordinates):,}")
 
-    if parameters.get("micromon_block") != "tomo-segmentation-closed":
-        tilt_metadata["spike_coordinates"] = spike_coordinates
-        if len(spike_coordinates) > 0:
-            logger.info(f"Total number of particles = {len(spike_coordinates):,}")
+    tilt_metadata["spike_coordinates"] = spike_coordinates
+    logger.info(f"Total number of particles = {len(spike_coordinates):,}")
 
     mpi_funcs, mpi_args = [ ], [ ]
     if ctffind_tilt:
