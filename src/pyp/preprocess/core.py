1617ef9f3de0471a5d60e97a57fd1751c1e26b7c
Alberto Bartesaghi <alberto@cs.duke.edu>
2024-09-23 09:06:08 -0400

    Cleanup conditional expression used to determine when to convert tilt-series to squares

--- src/pyp/preprocess/core.py
+++ src/pyp/preprocess/core.py
@@ -653,14 +653,14 @@ def read_tilt_series(
     # only need squared tilt-series when: 
     # 1. tiltseries alignment is not done yet
     # 2. squared aligned tiltseries needs to be generated (for producing downsampled tomogram or subvolume/virion extraction)
-    if not project_params.tiltseries_align_is_done(metadata) or \
-        not merge.tomo_is_done(name, os.path.join(project_path, "mrc")) or \
-        ( parameters["tomo_vir_method"] != "none" and parameters["detect_force"] ) or \
-        parameters["tomo_vir_force"] or \
-        parameters["tomo_rec_force"] or \
-        tomo_subvolume_extract_is_required(parameters) or \
-        tomo_vir_is_required(parameters) or \
-        not ctf_mod.is_done(metadata,parameters, name=name, project_dir=project_path):
+    if ( not project_params.tiltseries_align_is_done(metadata)
+        or not merge.tomo_is_done(name, os.path.join(project_path, "mrc")) 
+        or ( parameters["tomo_vir_method"] != "none" and parameters["detect_force"] )
+        or parameters["tomo_vir_force"] 
+        or parameters["tomo_rec_force"]
+        or tomo_subvolume_extract_is_required(parameters)
+        or tomo_vir_is_required(parameters)
+        or not ctf_mod.is_done(metadata,parameters, name=name, project_dir=project_path) ):
 
         t = timer.Timer(text="Convert tilt-series into squares took: {}", logger=logger.info)
         t.start()
