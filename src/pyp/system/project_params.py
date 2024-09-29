6883f4261e5b783340eede940daf830a8dc57da1
Alberto Bartesaghi <alberto@cs.duke.edu>
2024-09-28 23:17:32 -0400

    Do not set virion recalculation flag when picking virions manually in new particle picking block

--- src/pyp/system/project_params.py
+++ src/pyp/system/project_params.py
@@ -909,7 +909,16 @@ def parameter_force_check(previous_parameters, new_parameters, project_dir="."):
                         new_parameters["detect_force"] = True
                         clean_picking_files(project_dir)
 
-                elif ( "tomo_vir_" in k and "tomo_srf_detect_" not in k and "tomo_vir_detect_" not in k ) or ("tomo_spk_" in k and new_parameters["micromon_block"] == "tomo-picking") or "tomo_pick_" in k and ( new_parameters["micromon_block"] == "tomo-picking" or new_parameters["micromon_block"] == "tomo-segmentation-closed" ):
+                elif (
+                    ( "tomo_vir_" in k 
+                     and "tomo_srf_detect_" not in k 
+                     and "tomo_vir_detect_" not in k 
+                     and not ( new_parameters.get("micromon_block") == "tomo-picking" and new_parameters.get("tomo_pick_method") == "manual" )
+                    )
+                    or ("tomo_spk_" in k and new_parameters["micromon_block"] == "tomo-picking")
+                    or "tomo_pick_" in k and ( new_parameters["micromon_block"] == "tomo-picking" or new_parameters["micromon_block"] == "tomo-segmentation-closed" )
+                    or new_parameters.get("tomo_vir_method") == "pyp-eval" and new_parameters["micromon_block"] == "tomo-preprocessing" and "detect_nn3d_" in k
+                ):
                     logger.info(
                         f"Virions will be re-computed to reflect change in parameter {k}"
                     )
