949079b9277e863460da9bff8bad108b463ceff3
Alberto Bartesaghi <alberto@cs.duke.edu>
2024-09-29 15:10:17 -0400

    Remove extra ; from NN initialization commands

--- src/pyp/detect/joint.py
+++ src/pyp/detect/joint.py
@@ -22,8 +22,8 @@ logger = initialize_pyp_logger(log_name=relative_path)
 
 NN_INIT_COMMANDS = "export LD_LIBRARY_PATH=/opt/conda/envs/pyp/lib/python3.8/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH; export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python"
 
-NN_INIT_COMMANDS_2D = f"{NN_INIT_COMMANDS}; export PYTHONPATH=$PYTHONPATH:$PYP_DIR/external/spr_pick;"
-NN_INIT_COMMANDS_3D = f"{NN_INIT_COMMANDS}; export PYTHONPATH=$PYTHONPATH:$PYP_DIR/external/cet_pick;"
+NN_INIT_COMMANDS_2D = f"{NN_INIT_COMMANDS}; export PYTHONPATH=$PYTHONPATH:$PYP_DIR/external/spr_pick"
+NN_INIT_COMMANDS_3D = f"{NN_INIT_COMMANDS}; export PYTHONPATH=$PYTHONPATH:$PYP_DIR/external/cet_pick"
 
 def bin_image( input, output, binning, verbose ):
     command = f"{get_imod_path()}/bin/newstack '{input}' '{output}' -bin {binning}; rm -f '{output}~'"
