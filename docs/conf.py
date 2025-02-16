b6f3d1324d938d4326fe5a1a0bc4573790467128
Alberto Bartesaghi <alberto@cs.duke.edu>
2025-02-16 12:42:29 -0500

    Undo one more change due to unintentional merge of docs branch

--- docs/conf.py
+++ docs/conf.py
@@ -70,8 +70,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_immaterial"
-# html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_immaterial"
+html_theme = "sphinx_book_theme"
 # html_theme = "sphinx_rtd_theme"
 # html_theme_options = {'page_width': 'auto'}
 
