34b5121a364891003abb10b09c0477e9b4377cd1
Alberto Bartesaghi <alberto@cs.duke.edu>
2025-12-02 17:55:51 -0500

    Fix issue with pytorch compilation options when invoking commands from cet_pick repo

--- src/pyp/detect/joint.py
+++ src/pyp/detect/joint.py
@@ -167,6 +167,37 @@ def bin_next_coordinates(coordinates,binning):
     # overwrite original file with pyp coordinates
     np.savetxt(coordinates, coordinate_file, fmt='%s', delimiter='\t')
 
+def get_compilation_flags(mode,args):
+    """
+    Generate compilation flags based on the specified mode and arguments.
+
+    Args:
+        mode (str): The mode for which the compilation flags are being generated (nn3d or milo).
+        args (dict): A dictionary of arguments containing keys that determine 
+                        whether compilation is enabled and the compilation mode.
+
+                        Expected keys:
+                        - 'detect_<mode>_compile': A flag indicating if compilation is enabled.
+                        - 'detect_<mode>_compile_mode': A string representing the compilation mode.
+                        Possible values:
+                        - "0": Default compilation mode.
+                        - "1": Reduce-overhead compilation mode.
+                        - "2": Max-autotune compilation mode.
+
+    Returns:
+        str: A string containing the compilation flags. If compilation is not enabled 
+                for the given mode, an empty string is returned.
+    """
+    compilation = ""
+    if 'detect_' + mode + '_compile' in args:
+        compilation = f"--compile --compile_mode "
+        if args['detect_' + mode + '_compile_mode'] == "0":
+            compilation += "default "
+        elif args['detect_' + mode + '_compile_mode'] == "1":
+            compilation += "reduce-overhead "
+        elif args['detect_' + mode + '_compile_mode'] == "2":
+            compilation += "max-autotune "
+    return compilation
 
 def tomotrain(args):
     """Train NN for 3D particle picking.
@@ -330,16 +361,7 @@ def tomotrain(args):
         if args['detect_nn3d_impute_tomograms']:
             masking += "--impute_tomograms "
 
-    compilation = ""
-    if 'detect_nn3d_compile' in args:
-        compilation = f"--compile --compile_mode "
-
-        if args['detect_nn3d_compile_mode'] == 0:
-            compilation += "default "
-        elif args['detect_nn3d_compile_mode'] == 1:
-            compilation += "reduce-overhead "
-        elif args['detect_nn3d_compile_mode'] == 2:
-            compilation += "max-autotune "
+    compilation = get_compilation_flags('nn3d',args)
 
     command = f"{NN_INIT_COMMANDS_3D} python -u {os.environ['PYP_DIR']}/external/cet_pick/cet_pick/main.py semi --down_ratio {args['detect_nn3d_down_ratio']} {compress} {gpu} --num_epochs {args['detect_nn3d_num_epochs']} --bbox {args['detect_nn3d_bbox']} --translation_ratio {args['detect_nn3d_translation_ratio']} --contrastive --exp_id test_reprod --dataset semi --arch unet_4 {debug} --val_interval {args['detect_nn3d_val_interval']} --save_all --thresh {args['detect_nn3d_thresh']} --cr_weight {args['detect_nn3d_cr_weight']} --temp {args['detect_nn3d_temp']} --tau {args['detect_nn3d_tau']} --K {args['detect_nn3d_max_objects']} --lr {args['detect_nn3d_lr']} {masking}{compilation}--train_img_txt '{train_images}' --train_coord_txt '{train_coords}' --val_img_txt '{validation_images}' --val_coord_txt '{validation_coords}' --test_img_txt '{validation_images}' --test_coord_txt '{validation_coords}' 2>&1 | tee {os.path.join( os.getcwd(), 'log', time_stamp + '_cet_pick_train.log')}"
     local_run.stream_shell_command(command)
@@ -460,16 +482,7 @@ def tomoeval(args,name):
             if args['detect_nn3d_impute_tomograms']:
                 masking += "--impute_tomograms "
 
-        compilation = ""
-        if 'detect_nn3d_compile' in args:
-            compilation = f"--compile --compile_mode "
-
-            if args['detect_nn3d_compile_mode'] == 0:
-                compilation += "default "
-            elif args['detect_nn3d_compile_mode'] == 1:
-                compilation += "reduce-overhead "
-            elif args['detect_nn3d_compile_mode'] == 2:
-                compilation += "max-autotune "
+        compilation = get_compilation_flags('nn3d',args)
 
         command = f"{NN_INIT_COMMANDS_3D} python -u {os.environ['PYP_DIR']}/external/cet_pick/cet_pick/test.py semi --arch unet_4 --dataset semi {with_score} --exp_id test_reprod --load_model '{project_params.resolve_path(args['detect_nn3d_ref'])}' {compress} {gpu} {fiber} --down_ratio 2 --contrastive --translation_ratio {args['detect_nn3d_translation_ratio']} --K {args['detect_nn3d_max_objects']} --out_thresh {args['detect_nn3d_thresh']} {masking}{compilation}--test_img_txt '{os.path.join( os.getcwd(), imgs_file)}' --test_coord_txt '{os.path.join( os.getcwd(), test_file)}' 2>&1 | tee '{os.path.join(project_folder, 'train', name + '_testing.log')}'"
         local_run.stream_shell_command(command)
@@ -595,16 +608,7 @@ def milotrain(args):
     else:
         gpu = "--gpus -1"
 
-    compilation = ""
-    if 'detect_milo_compile' in args:
-        compilation = f"--compile --compile_mode "
-
-        if args['detect_milo_compile_mode'] == 0:
-            compilation += "default "
-        elif args['detect_milo_compile_mode'] == 1:
-            compilation += "reduce-overhead "
-        elif args['detect_milo_compile_mode'] == 2:
-            compilation += "max-autotune "
+    compilation = get_compilation_flags('milo',args)
 
     if 'detect_milo_surface' in args:
         logger.info(f"Training MiLoPYP's exploration module with surface constraint")
@@ -813,16 +817,7 @@ def miloeval(args):
         else:
             gpu = "--gpus -1"
 
-        compilation = ""
-        if 'detect_milo_compile' in args:
-            compilation = f"--compile --compile_mode "
-
-            if args['detect_milo_compile_mode'] == 0:
-                compilation += "default "
-            elif args['detect_milo_compile_mode'] == 1:
-                compilation += "reduce-overhead "
-            elif args['detect_milo_compile_mode'] == 2:
-                compilation += "max-autotune "
+        compilation = get_compilation_flags('milo',args)
 
         if 'detect_milo_surface' in args:
             logger.info(f"Evaluating MiLoPYP's exploration module with surface constraint using {Path(project_params.resolve_path(args['detect_milo_model'])).name}")
