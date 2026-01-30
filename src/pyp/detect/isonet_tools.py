7f618366fa0b82be4ea08a1bc9d253a1a3b8ecc3
Alperen Tupurtu <alperen.tupurtu@duke.edu>
2026-01-30 16:36:17 -0500

    fix: We do not need to copy tomograms twice, they are already copied while flipping before.

--- src/pyp/detect/isonet_tools.py
+++ src/pyp/detect/isonet_tools.py
@@ -505,7 +505,7 @@ def isonet_predict( name, project_dir, parameters ):
 def isonet2_predict( name, project_dir, parameters ):
     
     # transfer/convert tomogram to local scratch
-    convert_and_transfer_tomograms( [name], project_dir, parameters)
+    # convert_and_transfer_tomograms( [name], project_dir, parameters)
 
     initial_star = f"{name}_tomograms.star"
     isonet2_generate_star( project_dir, initial_star, parameters, name_list=[name])
@@ -561,7 +561,7 @@ def isonet2_train( project_dir, parameters):
     if parameters["tomo_denoise_isonet2_mask"]:
         
         # transfer/convert tomograms to local scratch
-        convert_and_transfer_tomograms( train_name, project_dir, parameters)
+        # convert_and_transfer_tomograms( train_name, project_dir, parameters)
     
         if parameters["tomo_denoise_isonet2_mask_preprocessing"] == "deconv":
 
@@ -594,8 +594,8 @@ def isonet2_train( project_dir, parameters):
             parameters=parameters
             )
 
-    if (not parameters["tomo_denoise_isonet2_mask"]) and (parameters["tomo_denoise_isonet2_refine_method"] == "isonet2"):
-        convert_and_transfer_tomograms(train_name, project_dir, parameters)
+    # if (not parameters["tomo_denoise_isonet2_mask"]) and (parameters["tomo_denoise_isonet2_refine_method"] == "isonet2"):
+    #     convert_and_transfer_tomograms(train_name, project_dir, parameters)
 
     # refine (train)
     output_dir = os.path.join( os.getcwd(), "isonet_maps")
