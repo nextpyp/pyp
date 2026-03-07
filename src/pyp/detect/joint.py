99e4d084b06d8cb54ef4c3f0075770acc538c49b
Alberto Bartesaghi <alberto@cs.duke.edu>
2026-03-07 00:44:42 -0500

    Fix environment variables when running milopyp executables

--- src/pyp/detect/joint.py
+++ src/pyp/detect/joint.py
@@ -19,7 +19,7 @@ from pyp.system.logging import logger
 
 from skimage.color import rgb2hsv, hsv2rgb
 
-NN_INIT_COMMANDS = "export LD_LIBRARY_PATH=/opt/conda/envs/milopyp/lib/python3.12/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH; export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python"
+NN_INIT_COMMANDS = "export LD_LIBRARY_PATH=/opt/pixi/milopyp/.pixi/envs/default/lib:/opt/pixi/milopyp/.pixi/envs/default/lib/python3.11/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH"
 
 milopyp_path = '/opt/pixi/milopyp/.pixi/envs/default'
 NN_INIT_COMMANDS_2D = f"{NN_INIT_COMMANDS}; export PYTHONPATH=$PYTHONPATH:$PYP_DIR/external/spr_pick; export TERM=xterm; export PATH={milopyp_path}/bin:$PATH; "
