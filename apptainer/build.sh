c6b3c9a24adb1e6e9c17441a0639c20bc18269e3
Alberto Bartesaghi <alberto@cs.duke.edu>
2026-03-29 11:03:30 -0400

    Update build script for container

--- apptainer/build.sh
+++ apptainer/build.sh
@@ -1,176 +1,58 @@
 #!/bin/bash
 
-# build.sh will build a Singularity container. It's not overly complicated.
-#
-# USAGE: build.sh --uri collection-name/container-name --cli registry Singularity
-#        build.sh --uri collection-name/container-name --cli registry
-#        build.sh Singularity
+git_cryoluge="/scratch/cryoluge"
+git_csp2="/scratch/csp2"
+git_mojit="/scratch/mojit"
+# TODO: replace these with git URLs
 
-# Copyright (C) 2017-2020 Vanessa Sochat.
+# find the project sources
+binds=()
 
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
-# License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-set -o errexit
-set -o nounset
-
-function usage() {
-
-    echo "USAGE: build [recipe] [options]"
-    echo ""
-    echo "OPTIONS:
-
-          Image Format
-              --uri   -u    if uploading, a uri to give to sregistry
-              --cli   -c    the sregistry client to use (if uploading)
-              --help  -h    show this help and exit
-              "
+addbind() {
+    binds+=(--bind "$1:$2")
 }
 
-# --- Option processing --------------------------------------------------------
-
-uri=""
-cli=""
-tag=""
-
-while true; do
-    case ${1:-} in
-        -h|--help|help)
-            usage
-            exit 0
-        ;;
-        -u|--uri)
-            shift
-            uri="${1:-}"
-            shift
-        ;;
-        -t|--tag)
-            shift
-            tag="${1:-}"
-            shift
-        ;;
-        -c|--cli)
-            shift
-            cli="${1:-}"
-            shift
-        ;;
-        \?) printf "illegal option: -%s\n" "${1:-}" >&2
-            usage
+foundat=
+findproject() {
+    case "$1" in
+        "https://"*)
+            foundat="$1"
+            ;;
+        "http://"*)
+            echo "ERROR: always download code with HTTPs, not HTTP  $1"
             exit 1
-        ;;
-        -*)
-            printf "illegal option: -%s\n" "${1:-}" >&2
-            usage
-            exit 1
-        ;;
+            ;;
         *)
-            break;
-        ;;
+            if [ -d "$1" ]; then
+                addbind "$1" "$2"
+                foundat="$2"
+            else
+                echo "ERROR: no folder found at: $1"
+                exit 1
+            fi
+            ;;
     esac
-done
-
-################################################################################
-### Recipe File ################################################################
-################################################################################
-
-
-if [ $# == 0 ] ; then
-    recipe="Singularity"
-else
-    recipe=$1
-fi
-
-echo ""
-echo "Image Recipe: ${recipe}"
-
-
-################################################################################
-### Storage Client #############################################################
-################################################################################
-
-is_valid_client () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
 }
 
-# Test if client is valid
+findproject "$git_cryoluge" "/src/cryoluge"
+git_cryoluge="$foundat"
 
-clients=("google-storage" "registry" "globus" "dropbox" "google-drive")
+findproject "$git_csp2" "/src/csp2"
+git_csp2="$foundat"
 
-if [ "${cli}" != "" ]; then
-    is_valid_client "${cli}" "${clients[@]}"
-    if [ $? -ne 0 ]; then
-        echo "${cli} is not a valid choice! Choose from ${clients[@]}";
-        exit 1
-    fi
-    echo "Storage Client: ${cli}"
-else
-    echo "Storage Client: none"
-fi
+findproject "$git_mojit" "/src/mojit"
+export git_mojit="$foundat"
 
+export APPTAINER_TMPDIR=/scratch/scratch
+export TMPDIR=/scratch/tmp
 
-################################################################################
-### Build! #####################################################################
-################################################################################
-
-# Continue if the image recipe is found
-
-if [ -f "$recipe" ]; then
-
-    imagefile="${recipe}.sif"
-    imagefile="pyp.sif"
-
-    echo "Creating $imagefile using $recipe..."
-    singularity build $imagefile $recipe
-
-    # If the image is successfully built, test it and upload (examples)
-  
-    if [ -f "${imagefile}" ]; then
-
-        # Example testing using run (you could also use test command)
-
-        echo "Testing the image... Marco!"
-        singularity exec $imagefile echo "Polo!"
-
-        # Example sregistry commands to push to endpoints
-
-        if [ "${cli}" != "" ]; then
-
-            # If the uri isn't provided, he gets a robot name
-            if [ "${uri}" == "" ]; then
-                uri=$(python -c "from sregistry.logger.namer import RobotNamer; bot=RobotNamer(); print(bot.generate())")
-            fi
-
-            # If a tag is provided, add to uri
-            if [ "${tag}" != "" ]; then
-                uri="${uri}:${tag}"
-            fi
-
-            echo "Pushing ${uri} to ${cli}://"
-            echo "SREGISTRY_CLIENT=${cli} sregistry push --name ${uri} ${imagefile}"
-            SREGISTRY_CLIENT="${cli}" sregistry push --name "${uri}" "${imagefile}"
-
-        else
-            echo "Skipping upload. Image $imagefile is finished!"
-        fi
-
-    fi
-
-else
-
-    echo "Singularity recipe ${recipe} not found!"
-    exit 1
-
-fi
+# build the app container
+apptainer build \
+    --force \
+    "${binds[@]}" \
+    --build-arg git_cryoluge="$git_cryoluge" \
+    --build-arg git_csp2="$git_csp2" \
+    --build-arg git_mojit="$git_mojit" \
+    -B /scratch:/tmp -B ~/.ssh/:/root \
+    pyp.sif \
+    apptainer/pyp.def
